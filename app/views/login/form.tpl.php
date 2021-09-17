dc48813a93d43a96539af89af2031317f58bdfe6
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-17 15:14:37 +0200

    login form view done

---
+++ app/views/login/form.tpl.php
@@ -0,0 +1,13 @@
+<h2>Connexion</h2>
+
+<form action="" method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Adresse e-mail</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Adresse e-mail&hellip;" required>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Mot de passe</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe&hellip;" required>
+  </div>
+  <button type="submit" class="btn btn-primary">Se connecter</button>
+</form>
