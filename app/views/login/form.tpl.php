ec15df27e0410b4904cbb0316464347edabc1ad6
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-17 15:16:30 +0200

    update form

--- app/views/login/form.tpl.php
+++ app/views/login/form.tpl.php
@@ -3,11 +3,11 @@
 <form action="" method="POST">
   <div class="form-group">
     <label for="exampleInputEmail1">Adresse e-mail</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Adresse e-mail&hellip;" required>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Adresse e-mail&hellip;" required>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Mot de passe</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe&hellip;" required>
+    <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Mot de passe&hellip;" required>
   </div>
   <button type="submit" class="btn btn-primary">Se connecter</button>
 </form>
