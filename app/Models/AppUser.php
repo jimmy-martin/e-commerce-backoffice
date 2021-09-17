1529123930a130b16bab88a4ffc8c721284a4500
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-17 14:07:48 +0200

    UserApp model created

---
+++ app/Models/AppUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Utils\Database;
+use PDO;
+
+class AppUser extends CoreModel
+{
+    private $email;
+    private $password;
+    private $firstname;
+    private $lastname;
+    private $role;
+    private $status;
+
+    
+}
