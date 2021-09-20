195f9c5abe26d9abdfa5ff0eae6a100b6f20cdea
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-20 12:20:58 +0200

    user list route and controller done

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\AppUser;
+
+class UserController extends CoreController
+{
+    /**
+     * List all users
+     *
+     * @return void
+     */
+    public function list()
+    {
+        $users = AppUser::findAll();
+
+        $this->show('user/list', [
+            'users' => $users
+        ]);
+    }
+}
